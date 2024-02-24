c77353da982bffd105eac92f101876474a65c6a5
supriyasupriyakonda <113300967+supriyasupriyakonda@users.noreply.github.com>
2024-02-24 13:50:05 +0530

    Add files via upload

---
+++ EXPERIMENT34.R
@@ -0,0 +1,6 @@
+set.seed(123)
+response_times <- rnorm(100, mean = 100, sd = 20)
+cumulative_times <- cumsum(response_times)
+plot(cumulative_times, type = "l", col = "blue", lwd = 2,
+     xlab = "Trials", ylab = "Cumulative Response Time",
+     main = "Cumulative Response Time Plot")
